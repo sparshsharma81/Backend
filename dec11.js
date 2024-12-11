7111f90a63925107a730abcb36be6a237120e9a1
sparsh <130464884+sparshsharma81@users.noreply.github.com>
2024-12-11 14:50:01 +0530

    11

---
+++ dec11.js
@@ -0,0 +1,9 @@
+console.log("Hii...to kaise hai aap log");
+console.log("Node js is an runtime environment of node js...")
+console.log("Node js is a free, open source, cross-platform javascript runtime environment that lets developers create servers");
+console.log("console.to use");
+/*
+
+cgs---old javascript
+mjs -- new version of javascript
+*/
